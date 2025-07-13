b7069da4f6fd34bae6169cbfb8462b344785e9fa
Eduardo Silveira <edupsilveira@gmail.com>
2025-07-12 22:10:01 -0300

    fix: questions itens are updating correctly and wainting the answer; sending to back-end chunks of audio

--- src/components/question-form.tsx
+++ src/components/question-form.tsx
@@ -48,6 +48,8 @@ export function QuestionForm({ roomId }: QuestionFormProps) {
     await createQuestion(data)
   }
 
+  const { isSubmitting } = form.formState
+
   return (
     <Card>
       <CardHeader>
@@ -71,6 +73,7 @@ export function QuestionForm({ roomId }: QuestionFormProps) {
                   <FormControl>
                     <Textarea
                       className="min-h-[100px]"
+                      disabled={isSubmitting}
                       placeholder="O que você gostaria de saber?"
                       {...field}
                     />
@@ -80,7 +83,9 @@ export function QuestionForm({ roomId }: QuestionFormProps) {
               )}
             />
 
-            <Button type="submit">Enviar pergunta</Button>
+            <Button disabled={isSubmitting} type="submit">
+              Enviar pergunta
+            </Button>
           </form>
         </Form>
       </CardContent>
--- src/pages/record-room-audio.tsx
+++ src/pages/record-room-audio.tsx
@@ -13,15 +13,19 @@ type RoomParams = {
 
 export function RecordRoomAudio() {
   const params = useParams<RoomParams>()
-
   const [isRecording, setIsRecording] = useState(false)
   const recorder = useRef<MediaRecorder | null>(null)
+  const intervalRef = useRef<NodeJS.Timeout>(null)
 
   function stopRecording() {
     setIsRecording(false)
 
     if (recorder.current && recorder.current.state !== 'inactive') {
       recorder.current.stop()
+    }
+
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current)
     }
   }
 
@@ -42,22 +46,8 @@ export function RecordRoomAudio() {
     // biome-ignore lint/suspicious/noConsole: TESTE
     console.log('Audio uploaded:', result)
   }
-  async function startRecording() {
-    if (!isRecordingSupported) {
-      alert('Gravação de áudio não é suportada neste navegador.')
-      return
-    }
 
-    setIsRecording(true)
-
-    const audio = await navigator.mediaDevices.getUserMedia({
-      audio: {
-        echoCancellation: true,
-        noiseSuppression: true,
-        sampleRate: 44_100,
-      },
-    })
-
+  function createRecorder(audio: MediaStream) {
     recorder.current = new MediaRecorder(audio, {
       mimeType: 'audio/webm',
       audioBitsPerSecond: 64_000,
@@ -80,6 +70,31 @@ export function RecordRoomAudio() {
     }
 
     recorder.current.start()
+  }
+
+  async function startRecording() {
+    if (!isRecordingSupported) {
+      alert('Gravação de áudio não é suportada neste navegador.')
+      return
+    }
+
+    setIsRecording(true)
+
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44_100,
+      },
+    })
+
+    createRecorder(audio)
+
+    intervalRef.current = setInterval(() => {
+      recorder.current?.stop()
+
+      createRecorder(audio)
+    }, 5000)
   }
 
   if (!params.roomId) {
